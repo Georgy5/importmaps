d2944a0e417a55792c3cdb8761d8d30b64034c02
Karl Keller <kgk86@hotmail.com>
2023-01-28 14:25:24 +0100

    Update RecipesControllerTest

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,7 +2,14 @@ require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get recipes_index_url
+    get recipes_path
     assert_response :success
+  end
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Rezeptbuch"
+    assert_select "h1", /^Willkommen/
   end
 end
