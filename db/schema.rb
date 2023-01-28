d4f184e07a685122dc9aff632cc12f32e24edffc
Karl Keller <kgk86@hotmail.com>
2023-01-28 15:38:30 +0100

    Generate User model and Users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_131849) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_143648) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
