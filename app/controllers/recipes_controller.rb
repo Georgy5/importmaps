f4872dd26cb97b8b7d517c3c30a796c89bc78d6b
Karl Keller <kgk86@hotmail.com>
2023-01-28 15:33:18 +0100

    Show recipes feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all.order("created_at DESC")
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 end
