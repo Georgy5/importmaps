aa6932b779b00caa5dcd0ab457e192d7d3e6361a
Karl Keller <kgk86@hotmail.com>
2023-01-28 16:09:13 +0100

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_150631) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_150831) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_150631) do
     t.datetime "updated_at", null: false
     t.string "user"
     t.string "references"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_150631) do
     t.string "last_name"
   end
 
+  add_foreign_key "recipes", "users"
 end
