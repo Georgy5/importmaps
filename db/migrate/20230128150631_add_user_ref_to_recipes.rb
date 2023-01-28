06d7d180a9af8096256a58bbd7e6d91cdc690481
Karl Keller <kgk86@hotmail.com>
2023-01-28 16:07:09 +0100

    Add User reference to Recipes

---
+++ db/migrate/20230128150631_add_user_ref_to_recipes.rb
@@ -0,0 +1,6 @@
+class AddUserRefToRecipes < ActiveRecord::Migration[7.0]
+  def change
+    add_column :recipes, :user, :string
+    add_column :recipes, :references, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_145949) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_150631) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user"
+    t.string "references"
   end
 
   create_table "users", force: :cascade do |t|
